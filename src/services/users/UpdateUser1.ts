8ef9396e4eb5b175f39039c72c246069152b2476
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2025-03-25 20:32:23 +0100

    fix name

---
+++ src/services/users/UpdateUser1.ts
@@ -0,0 +1,29 @@
+export const updateUser = async (userId: string, userData: any) => {
+  //   const response = await fetch(
+  //     `http://localhost:5001/auth/updateUser/${userId}`,
+  //     {
+  //       method: "Put",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Accept: "application/json",
+  //       },
+  //       body: JSON.stringify(userData),
+  //     }
+  //   );
+  //   const data = await response.json();
+
+  //   return {
+  //     success: response.status === 200,
+  //     message: data.message,
+  //     data: data.data,
+  //   };
+  return {
+    success: true,
+    message: "User updated successfully",
+    data: { ...userData, _id: userId },
+  };
+};
+//const Somme = (x:number,y:number) => {
+// return x + y
+//}
+//Somme(5,2)
